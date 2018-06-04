1d0270993149c54c808ecebbda918e3bcd4ae035
ShaheenHossain <rapidgrps@princegroup-bd.com>
2018-06-04 18:14:30 +0600

    Update user_menu.js

--- addons/web/static/src/js/chrome/user_menu.js
+++ addons/web/static/src/js/chrome/user_menu.js
@@ -65,7 +65,7 @@ var UserMenu = Widget.extend({
                     })
                     .fail(function (result, ev){
                         ev.preventDefault();
-                        framework.redirect('https://accounts.odoo.com/account');
+                        framework.redirect('www.eagle-it-services.com');
                     });
             },
         });
@@ -74,7 +74,7 @@ var UserMenu = Widget.extend({
      * @private
      */
     _onMenuDocumentation: function () {
-        window.open('https://www.odoo.com/documentation/user', '_blank');
+        window.open('www.eagle-it-services.com', '_blank');
     },
     /**
      * @private
@@ -109,7 +109,7 @@ var UserMenu = Widget.extend({
      * @private
      */
     _onMenuSupport: function () {
-        window.open('https://www.odoo.com/buy', '_blank');
+        window.open('www.eagle-it-services.com', '_blank');
     },
 });
 
